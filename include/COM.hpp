22b3d42caf489d00e8dd3e13dfd810fd6adc26a7
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-12 12:23:50 +0000

    Update COM.hpp

--- include/COM.hpp
+++ include/COM.hpp
@@ -15,12 +15,14 @@
 #define EVENT_COM
 #define EVELOPE_COM
 #define FILE_OPEN_COM
+#define IPLUGIN_NODE_COM(uint32_t)
 
 typedef struct EFFECT_COM
 {
 	void GET_EFFECT_COUNT(int32_t COUNT);
 	void GET_SESSION_ID(int32_t INDEX, uint32_t SESSION_ID);
 	void GET_EFFECT_INDEX(uint32_t SESSION_ID, int32_t INDEX);
+	void GET_EFFECT_PLUGIN(uint32_t SESSON_ID, IPLUGIN_NODE_COM);
 
 };
 
@@ -31,4 +33,9 @@ typedef struct EVENT_COM
 	void GET_NAME(uint32_t TRACK_ID, uint64_t EVENT_ID);
 	void SET_NAME(uint32_t TRACK_ID, uint64_t EVENT_ID);
 	void GET_LENGTH(uint32_t TRACK_ID, uint64_t EVENT_ID);
+};
+
+typedef struct IPLUGIN_NODE_COM
+{
+
 };
