2791282faaa946f715221c9acbc6a50d2f2a9c4a
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-17 22:35:06 +0000

    Update COM.hpp

--- include/COM.hpp
+++ include/COM.hpp
@@ -9,14 +9,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define BUS_TRACK_COM
-#define COM
-#define EFFECT_COM
-#define EVENT_COM
-#define EVELOPE_COM
-#define FILE_OPEN_COM
-#define IPLUGIN_NODE_COM(uint32_t)
-
 typedef struct EFFECT_COM
 {
 	void GET_EFFECT_COUNT(int32_t COUNT);
@@ -25,6 +17,9 @@ typedef struct EFFECT_COM
 	void GET_EFFECT_PLUGIN(uint32_t SESSON_ID, IPLUGIN_NODE_COM);
 	void ADD_EFFECT(IPLUGIN_NODE_COM COM, int32_t INDEX, uint32_t SESSION_ID);
 	void REMOVE_EFFECT(uint32_t SESSION_ID);
+	void GET_PRESET_COUNT(uint32_t SESSION_ID, int32_t COUNT);
+	void GET_PRESET_NAME(uint32_t SESSION_ID, int32_t INDEX, char* NAME);
+	void GET_CURRENT_PRESET();
 
 };
 
@@ -34,10 +29,11 @@ typedef struct EVENT_COM
 	void GET_MEDIA_TYPE(uint32_t TRACK_ID, uint64_t EVENT_ID);
 	void GET_NAME(uint32_t TRACK_ID, uint64_t EVENT_ID);
 	void SET_NAME(uint32_t TRACK_ID, uint64_t EVENT_ID);
+	void GET_START(uint32_t TRACK_ID, );
 	void GET_LENGTH(uint32_t TRACK_ID, uint64_t EVENT_ID);
 };
 
-struct IPLUGIN_NODE_COM
+typedef struct IPLUGIN_NODE_COM
 {
 	void GET_COUNT(int32_t COUNT);
 	void GET_CHILD_BY_INDEX(int32_t INDEX, IPLUGIN_NODE_COM CHILD);
