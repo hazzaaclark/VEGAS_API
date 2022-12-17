1abbec8523adad1501bac5857deefb7a8321dd56
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-17 23:54:06 +0000

    Update pch.h

--- include/pch.h
+++ include/pch.h
@@ -9,3 +9,17 @@
 #include "COM.hpp"
 
 #endif
+
+#ifdef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+/* BASED ON THE CROSS-COMPATIBILITY */
+/* OF THE DETOURS WIN_API MODULE */
+
+#ifdef __cplusplus
+extern "C"
+{
+
+}
+#endif
