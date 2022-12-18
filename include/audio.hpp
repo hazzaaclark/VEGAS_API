b252dd132b7aafafb01b74c2619850b88493d444
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-12-18 01:46:48 +0000

    Update audio.hpp

--- include/audio.hpp
+++ include/audio.hpp
@@ -21,8 +21,10 @@ typedef struct
 	static bool GET_NORMALISE;
 	static bool RESULT;
 	static bool SET_NORM;
+	static bool FORMAT_LOCK;
 	static double NORMALISE_GAIN;
 	static double GAIN_RESULT;
+	static double FORMAT_SHIFT;
 
 } AUDIO_EVENT;
 
@@ -48,4 +50,47 @@ typedef enum CHANNEL_MAPPING
 	MUTE_R,
 	MONO,
 	SWAP
+};
+
+typedef enum TIME_STRETCH_PITCH_SHIFT
+{
+	CLASSIC = 1,
+	ACID = 2,
+	ELASTIQUE = 3,
+	NONE
+};
+
+typedef struct STRETCH_ATTRIBUTES
+{
+	enum CLASSIC
+	{
+		A1,
+		A2,
+		A3,
+		A4,
+		A5,
+		A6,
+		A7,
+		A8,
+		A9,
+		A10,
+		A11,
+		A12,
+		A13,
+		A14,
+		A15,
+		A16,
+		A17,
+		A18,
+		A19
+	};
+
+	enum ELASTIQUE
+	{
+		PRO,
+		EFFICIENT,
+		SOLOIST_MONOPHONIC,
+		SOLOIST_SPEECH
+	};
+
 };
